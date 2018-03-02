a021f1c0e126312596cfc99a5b101ace22c0aa14
Josue Ramirez <joshua.rg18@gmail.com>
2018-03-01 19:05:18 -0600

    Create 09.NumerosTryCatch.cs

---
+++ 09.NumerosTryCatch.cs
@@ -0,0 +1,26 @@
+using System;
+class Numeros
+{
+	static void Main(string [] args)
+	{
+		int num, suma=0, cont=0;
+		Console.Write("Digite un numero: ");
+		try
+		{
+			num=Convert.ToInt32(Console.ReadLine());
+			while (num!=0)
+			{
+				suma+=num;
+				cont++;
+				Console.Write("Digite otro numero o cero para salir: ");
+				num=Convert.ToInt32(Console.ReadLine());
+			}
+			Console.WriteLine("La suma es: " + suma);
+			Console.WriteLine("La cantidad de numeros es: " + cont);
+		}
+		catch
+		{
+			Console.WriteLine("Dato incorrecto!!");
+		}
+	}	
+}
